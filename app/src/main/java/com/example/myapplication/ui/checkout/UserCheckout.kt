2d38c49a9f8273f1c5135dd5eeb84f3604ca9a00
Javeria Baloch <65242641+JaveriaBaloch@users.noreply.github.com>
2023-02-19 20:07:58 +0100

    all done

--- app/src/main/java/com/example/myapplication/ui/checkout/UserCheckout.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.ui.checkout
-
-import com.example.myapplication.ui.home.itemClass
-import org.json.JSONArray
-
-
-data class UserCheckout(val fullname:String?=null,val phone:String?=null,val email:String?=null,val address:String?=null,val additionalnotes: String?=null,  val cartItems: List<itemClass>? = null ,  val currentDate: String? = null, var totalValue: Double?= null){
-
-}
