4623f519e6666df14fb0cfd877c3b78eb3f9043c
Javeria Baloch <65242641+JaveriaBaloch@users.noreply.github.com>
2023-11-13 21:22:04 +0100

    login

---
+++ Downloads/JKK 3/app/src/main/java/com/example/jkk/ui/home/HomeFragment.java
@@ -0,0 +1,31 @@
+package com.example.jkk.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.jkk.R;
+
+public class HomeFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        View root = inflater.inflate(R.layout.home_fragment,container,false
+        );
+
+
+        return root;
+
+    }
+
+
+
+}
